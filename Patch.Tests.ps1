d2a77a8135f5441ab7d8d552e0b3e17ff1bd2977
phreno <losgoomy@gmail.com>
2025-02-15 02:59:24 +0100

    feat: Ajoute un test pour la récupération du repository Git

--- Patch.Tests.ps1
+++ Patch.Tests.ps1
@@ -2,8 +2,7 @@ BeforeAll {
     Import-Module $PSCommandPath.Replace('.Tests.ps1', '.psd1') -Force
 }
 
-Describe "Applique la configuration" {
-    It "Recupere un objet de configuration" {
-        New-PatchConfiguration | Should -BeOfType HashTable
-    }
+Describe "Construit la configuration" {
+    It "Recupere un objet de configuration" { New-PatchConfiguration | Should -BeOfType HashTable }
+    It "Retourne le repository git cible" { (New-PatchConfiguration).Repository | Should -BeOfType System.IO.DirectoryInfo }
 }
--- Patch.psm1
+++ Patch.psm1
@@ -1,4 +1,4 @@
 function New-Configuration {
-    @{}
+    @{ Repository = Get-Item "./" }
 }
 
