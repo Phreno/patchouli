921bce414d49b1b4847974298e06b006e562f506
phreno <losgoomy@gmail.com>
2025-02-15 04:07:49 +0100

    test: Ajoute des tests pour la propriété Repository

--- Patch.Tests.ps1
+++ Patch.Tests.ps1
@@ -4,12 +4,14 @@ BeforeAll {
 
 Describe "La configuration du patch" {
     It "Recupere un objet de configuration" { New-PatchConfiguration | Should -BeOfType HashTable }
-    It "Retourne le repository git cible" { (New-PatchConfiguration).Repository | Should -BeOfType System.IO.DirectoryInfo }
-    It "Permet la Modification du repository git cible" {
-        (New-PatchConfiguration -Repository $PSScriptRoot).Repository.BaseName | Should -Be patchouli
-        (New-PatchConfiguration -Repository "../").Repository.BaseName | Should -Not -Be patchouli 
-    }
-    It "Assure que le repository git cible soit valide" {
-        "$((New-PatchConfiguration).Repository)/.git" | Test-Path | Should -Be $True 
+    Describe "La propriete Repository" {
+        It "Retourne le repository git cible" { (New-PatchConfiguration).Repository | Should -BeOfType System.IO.DirectoryInfo }
+        It "Permet la Modification du repository git cible" {
+            (New-PatchConfiguration -Repository $PSScriptRoot).Repository.BaseName | Should -Be patchouli
+            (New-PatchConfiguration -Repository "../").Repository.BaseName | Should -Not -Be patchouli 
+        }
+        It "Assure que le repository git cible soit valide" {
+            "$((New-PatchConfiguration).Repository)/.git" | Test-Path | Should -Be $True 
+        }
     }
 }
