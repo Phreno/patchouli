aa9362373d1d5ed098439ff36cc4a0484b21835d
phreno <losgoomy@gmail.com>
2025-02-15 03:08:14 +0100

    feat: Ajoute une commande de commit automatisée

--- Patch.Tests.ps1
+++ Patch.Tests.ps1
@@ -5,5 +5,5 @@ BeforeAll {
 Describe "Construit la configuration" {
     It "Recupere un objet de configuration" { New-PatchConfiguration | Should -BeOfType HashTable }
     It "Retourne le repository git cible" { (New-PatchConfiguration).Repository | Should -BeOfType System.IO.DirectoryInfo }
-    It "Permet la Modification du repository git cible" { (New-PatchConfiguration -Repository "./").Repository.BaseName | Should -Be patchouli }
+    It "Permet la Modification du repository git cible" { (New-PatchConfiguration -Repository "./patchouli").Repository.BaseName | Should -Be patchouli }
 }
