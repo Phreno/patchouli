c174a9822f0798ef9866e9919116bafe27f8e5cc
phreno <losgoomy@gmail.com>
2025-02-15 02:51:18 +0100

    Add Patch module with configuration and tests

---
+++ Patch.Tests.ps1
@@ -0,0 +1,9 @@
+BeforeAll {
+    Import-Module $PSCommandPath.Replace('.Tests.ps1', '.psd1') -Force
+}
+
+Describe "Applique la configuration" {
+    It "Recupere un objet de configuration" {
+        New-PatchConfiguration | Should -BeOfType HashTable
+    }
+}
