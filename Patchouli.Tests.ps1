3ae3b031197a939729e50293877d74d07959d09d
phreno <losgoomy@gmail.com>
2025-02-16 10:14:57 +0100

    test: Corrige les tests de configuration du patch

--- Patchouli.Tests.ps1
+++ Patchouli.Tests.ps1
@@ -6,17 +6,16 @@ Describe "La configuration du patch" {
     It "Recupere un objet de configuration" { New-PatchConfiguration | Should -BeOfType HashTable }
     Describe "La propriete Repository" {
         Context "Le repository git est valide" {
-            BeforeEach { Mock -ModuleName Patchouli Test-Path {return $true} }
+            BeforeEach { Mock -ModuleName Patchouli Test-Path { return $true } }
             It "Retourne le repository git cible" { (New-PatchConfiguration).Repository | Should -BeOfType System.IO.DirectoryInfo }
             It "Permet la Modification du repository git cible" {
-                (New-PatchConfiguration -Repository $PSScriptRoot).Repository.BaseName | Should -Be patchouli
+                (New-PatchConfiguration -Repository "./").Repository.BaseName | Should -Be patchouli
                 (New-PatchConfiguration -Repository "../").Repository.BaseName | Should -Not -Be patchouli 
             }
         }
         Context "Le repository git est invalide" {
-            # BeforeEach { Mock -ModuleName Patchouli Test-Path {return $true} }
             It "Intercepte une erreur" {
-                {New-PatchConfiguration -Repository "/this/directory/does/not/exists" } | Should -Throw
+                { New-PatchConfiguration -Repository "/this/directory/does/not/exists" } | Should -Throw
             }
         }
     }
